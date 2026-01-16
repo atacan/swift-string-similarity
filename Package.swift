7aefb85abd04b8a75631170d4e1ca41ea1a30501
atacan <atacandur@icloud.com>
2026-01-16 21:53:46 +0100

    free functions

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftStringSimilarity",
-            targets: ["SwiftStringSimilarity"]
+            name: "StringSimilarity",
+            targets: ["StringSimilarity"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftStringSimilarity"
+            name: "StringSimilarity"
         ),
         .testTarget(
-            name: "SwiftStringSimilarityTests",
-            dependencies: ["SwiftStringSimilarity"]
+            name: "StringSimilarityTests",
+            dependencies: ["StringSimilarity"]
         ),
     ]
 )
